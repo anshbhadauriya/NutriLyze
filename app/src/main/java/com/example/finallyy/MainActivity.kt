19f8f9f0610421a5287abc4324877a779c826321
Lenovo <2k23.csai2310627@gmail.com>
2025-06-16 19:39:16 +0530

    Initial commit

--- app/src/main/java/com/example/finallyy/MainActivity.kt
+++ app/src/main/java/com/example/finallyy/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
-
+//comment
         loginButton.setOnClickListener {
             val email = username.text.toString()
             val password2 = password.text.toString()
